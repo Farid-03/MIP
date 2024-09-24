d37d4249c5c2b53639aa903030dcad78c4c12046
Farid Ibrahimzada <ibrahimzadef26@gmail.com>
2024-09-24 15:35:19 +0400

    Service optimization

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,14 +10,28 @@ class MarvelService {
 
 		return await res.json()
 	}
-	getAllCharacters = () => {
-		return this.getResource(
+	getAllCharacters = async () => {
+		const res = await this.getResource(
 			`${this._apiBase}characters?limit=9&offset=211&${this._apiKey}`
 		)
+		return this._transformCharacter(res.data.results[0])
 	}
 
-	getCharacter = id => {
-		return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`)
+	getCharacter = async id => {
+		const res = await this.getResource(
+			`${this._apiBase}characters/${id}?${this._apiKey}`
+		)
+		return this._transformCharacter(res.data.results[0])
+	}
+
+	_transformCharacter = char => {
+		return {
+			name: char.name,
+			description: char.description,
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+		}
 	}
 }
 
